549a7e880151f250335519d7a3bcafceaec98d82
知行合一 <tom_9163@outlook.com>
2019-08-01 16:01:00 +0800

    Add keyboard input.t

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
 from Spider import ShanbayBook
 from WriteExcel import Sheet
 
-book = ShanbayBook(205950,643548,9,'./word.html')
+book_id = int(input('输入book_id:'))
+list_init_id = int(input('输入list_init_id:'))
+list_number = int(input('输入list个数:'))
+excel_name = input('输入excel表格名称，存放在当前文件夹下:')
+
+book = ShanbayBook(book_id,list_init_id,list_number,'./word.html')
 book.crawl_book()
 
-sheet = Sheet('./扇贝四级核心词汇.xls',book.output_location)
+sheet = Sheet(excel_name,book.output_location)
 sheet.writeExcel()
--- WriteExcel.py
+++ WriteExcel.py
@@ -1,6 +1,7 @@
 import re
-
+import os
 import xlwt
+import Spider
 
 
 class Sheet():
